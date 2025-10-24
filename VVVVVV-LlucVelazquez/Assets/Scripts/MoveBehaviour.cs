318b41033cb11caf33abd1ae8157caaa795b64cc
LlucVelazquez <lluc.velazquez.7e8@itb.cat>
2025-10-24 14:44:17 +0200

    Move Script Doned

---
+++ VVVVVV-LlucVelazquez/Assets/Scripts/MoveBehaviour.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class MoveBehaviour : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    public float speed;
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+    public void MoveCharacter(Vector2 direction)
+    {
+        _rb.linearVelocityX = direction.x * speed;
+    }
+    //public void ChangeGravity()
+}
---
+++ VVVVVV-LlucVelazquez/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+[RequireComponent(typeof(MoveBehaviour))]
+public class Player : MonoBehaviour, InputSystem_Actions.IPlayerActions
+{
+    private MoveBehaviour _mb;
+    private InputSystem_Actions inputActions;
+    private void Awake()
+    {
+        _mb = GetComponent<MoveBehaviour>();
+        inputActions = new InputSystem_Actions();
+        inputActions.Player.SetCallbacks(this);
+    }
+    private void OnEnable()
+    {
+        inputActions.Enable();
+
+    }
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        _mb.MoveCharacter(context.ReadValue<Vector2>());
+    }
+}
