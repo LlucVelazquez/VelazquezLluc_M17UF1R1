6e84f72434ed37330bbe0b01b580ef941902f151
LlucVelazquez <lluc.velazquez.7e8@itb.cat>
2025-11-07 15:53:36 +0100

    Parallax Effect Graveyard

---
+++ VVVVVV-LlucVelazquez/Assets/Scripts/ParallaxEffect.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class ParallaxEffect : MonoBehaviour
+{
+    private float _startingPos;
+    private float _lengthOfSprite;
+    public float AmountOfParallax;
+    public Camera MainCamera;
+
+    private void Start()
+    {
+        _startingPos = transform.position.x;
+        _lengthOfSprite = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+    private void Update()
+    {
+        Vector3 Position = MainCamera.transform.position;
+        float Temp = Position.x * (1 - AmountOfParallax);
+        float Distance = Position.x * AmountOfParallax;
+
+        Vector3 NewPosition = new Vector3(_startingPos + Distance, transform.position.y, transform.position.z);
+
+        transform.position = NewPosition;
+
+        if (Temp > _startingPos + (_lengthOfSprite / 2))
+        {
+            _startingPos += _lengthOfSprite;
+        }
+        else if (Temp < _startingPos - (_lengthOfSprite / 2))
+        {
+            _startingPos -= _lengthOfSprite;
+        }
+    }
+}
